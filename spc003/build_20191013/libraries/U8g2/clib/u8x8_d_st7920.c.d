e7771d2720e573810d4038d37f0c0677eb341119
Rhalf Wendel David Caacbay <rhalfcaacbay@gmail.com>
2019-10-13 03:52:39 +0800

    20191013
    -updated

---
+++ spc003/build_20191013/libraries/U8g2/clib/u8x8_d_st7920.c.d
@@ -0,0 +1,3 @@
+C:\Users\rhalf\AppData\Local\Temp\arduino_build_328292\libraries\U8g2\clib\u8x8_d_st7920.c.o: \
+ C:\Users\rhalf\Documents\Arduino\libraries\U8g2\src\clib\u8x8_d_st7920.c \
+ C:\Users\rhalf\Documents\Arduino\libraries\U8g2\src\clib\u8x8.h
